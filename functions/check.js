9e6fe1ac070af6b809c4e964135abba67c5a3ec4
SirPeca <159868571+SirPeca@users.noreply.github.com>
2026-02-03 14:43:50 -0300

    Update check.js

--- functions/check.js
+++ functions/check.js
@@ -1,183 +1,18 @@
-//0======================= CATALOGO DE TECNOLOGIAS =======================
-const CATALOG_UPDATE_DATE = "2026-02-03"; // fecha de última actualización del catálogo
-const CATALOG = {
-  "jquery": { name: "jQuery", key: "jquery" },
-  "jquery ui": { name: "jQuery UI", key: "jquery ui" },
-  "jquery blockui": { name: "jQuery BlockUI", key: "jquery blockui" },
-  "bootstrap": { name: "Bootstrap", key: "bootstrap" },
-  "angular": { name: "Angular", key: "angular" },
-  "react": { name: "React", key: "react" },
-  "vue": { name: "Vue.js", key: "vue" },
-  "vuex": { name: "Vuex", key: "vuex" },
-  "nuxt": { name: "Nuxt.js", key: "nuxt" },
-  "svelte": { name: "Svelte", key: "svelte" },
-  "moment.js": { name: "Moment.js", key: "moment" },
-  "moment-timezone": { name: "Moment Timezone", key: "moment-timezone" },
-  "toastr": { name: "Toastr", key: "toastr" },
-  "select2": { name: "Select2", key: "select2" },
-  "highcharts": { name: "Highcharts", key: "highcharts" },
-  "chart.js": { name: "Chart.js", key: "chart.js" },
-  "d3": { name: "D3.js", key: "d3" },
-  "axios": { name: "Axios", key: "axios" },
-  "socket.io": { name: "Socket.IO", key: "socket.io" },
-  "lodash": { name: "Lodash", key: "lodash" },
-  "underscore": { name: "Underscore.js", key: "underscore" },
-  "core-js": { name: "Core-js", key: "core-js" },
-  "tailwindcss": { name: "Tailwind CSS", key: "tailwindcss" },
-  "material-ui": { name: "Material-UI", key: "material-ui" },
-  "ant-design": { name: "Ant Design", key: "ant-design" },
-  "webpack": { name: "Webpack", key: "webpack" },
-  "babel": { name: "Babel", key: "babel" },
-  "typescript": { name: "TypeScript", key: "typescript" },
-  "sass": { name: "Sass", key: "sass" },
-  "less": { name: "Less", key: "less" },
-  "three.js": { name: "Three.js", key: "three.js" },
-  "animejs": { name: "Anime.js", key: "animejs" },
-  "gsap": { name: "GSAP", key: "gsap" },
-  "backbone": { name: "Backbone.js", key: "backbone" },
-  "ember": { name: "Ember.js", key: "ember" },
-  "p5": { name: "p5.js", key: "p5" },
-  "turbolinks": { name: "Turbolinks", key: "turbolinks" },
-  "crypto-js": { name: "CryptoJS", key: "crypto-js" },
-  "openssl": { name: "OpenSSL", key: "openssl" },
-  "node.js": { name: "Node.js", key: "node.js" },
-  "php": { name: "PHP", key: "php" },
-  "django": { name: "Django", key: "django" },
-  "laravel": { name: "Laravel", key: "laravel" },
-  "spring": { name: "Spring Framework", key: "spring" },
-  "ruby on rails": { name: "Ruby on Rails", key: "rails" },
-  "express": { name: "Express", key: "express" },
-  "flask": { name: "Flask", key: "flask" },
-  "matomo": { name: "Matomo Analytics", key: "matomo" },
-  "font awesome": { name: "Font Awesome", key: "fontawesome" },
-  "microsoft asp.net": { name: "ASP.NET", key: "asp.net" },
-  "hsts": { name: "HSTS", key: "hsts" },
-  "rxjs": { name: "RxJS", key: "rxjs" },
-  "immer": { name: "Immer", key: "immer" },
-  "recoil": { name: "Recoil", key: "recoil" },
-  "zustand": { name: "Zustand", key: "zustand" },
-  "formik": { name: "Formik", key: "formik" },
-  "yup": { name: "Yup", key: "yup" },
-  "joi": { name: "Joi", key: "joi" },
-  "react-query": { name: "React Query", key: "react-query" },
-  "react-router": { name: "React Router", key: "react-router" },
-  "next.js": { name: "Next.js", key: "next" },
-  "gatsby": { name: "Gatsby", key: "gatsby" },
-  "vite": { name: "Vite", key: "vite" },
-  "parcel": { name: "Parcel", key: "parcel" },
-  "rollup": { name: "Rollup", key: "rollup" },
-  "eslint": { name: "ESLint", key: "eslint" },
-  "prettier": { name: "Prettier", key: "prettier" },
-  "stylelint": { name: "Stylelint", key: "stylelint" },
-  "jest": { name: "Jest", key: "jest" },
-  "mocha": { name: "Mocha", key: "mocha" },
-  "chai": { name: "Chai", key: "chai" },
-  "cypress": { name: "Cypress", key: "cypress" },
-  "playwright": { name: "Playwright", key: "playwright" },
-  "puppeteer": { name: "Puppeteer", key: "puppeteer" },
-  "karma": { name: "Karma", key: "karma" },
-  "protractor": { name: "Protractor", key: "protractor" },
-  "nightwatch": { name: "Nightwatch", key: "nightwatch" },
-  "webdriverio": { name: "WebdriverIO", key: "webdriverio" },
-  "capybara": { name: "Capybara", key: "capybara" },
-  "selenium": { name: "Selenium", key: "selenium" },
-  "cucumber": { name: "Cucumber", key: "cucumber" },
-  "chai-http": { name: "Chai HTTP", key: "chai-http" },
-  "supertest": { name: "Supertest", key: "supertest" },
-  "nvd": { name: "NVD", key: "nvd" },
-  "endoflife.date": { name: "EndOfLife.date", key: "endoflife.date" },
-  "tailwind": { name: "Tailwind CSS", key: "tailwindcss" }
-};
+// ======================= CATALOGO DE TECNOLOGIAS =======================
+const CATALOG_UPDATE_DATE = "2026-02-03";
 
-//  ======================= CATALOGO DE TECNOLOGIAS =======================
-const CATALOG_UPDATE_DATE = "2026-02-03"; // fecha de última actualización del catálogo
 const CATALOG = {
-  "jquery": { name: "jQuery", key: "jquery" },
-  "jquery ui": { name: "jQuery UI", key: "jquery ui" },
-  "jquery blockui": { name: "jQuery BlockUI", key: "jquery blockui" },
-  "bootstrap": { name: "Bootstrap", key: "bootstrap" },
-  "angular": { name: "Angular", key: "angular" },
-  "react": { name: "React", key: "react" },
-  "vue": { name: "Vue.js", key: "vue" },
-  "vuex": { name: "Vuex", key: "vuex" },
-  "nuxt": { name: "Nuxt.js", key: "nuxt" },
-  "svelte": { name: "Svelte", key: "svelte" },
-  "moment.js": { name: "Moment.js", key: "moment" },
-  "moment-timezone": { name: "Moment Timezone", key: "moment-timezone" },
-  "toastr": { name: "Toastr", key: "toastr" },
-  "select2": { name: "Select2", key: "select2" },
-  "highcharts": { name: "Highcharts", key: "highcharts" },
-  "chart.js": { name: "Chart.js", key: "chart.js" },
-  "d3": { name: "D3.js", key: "d3" },
-  "axios": { name: "Axios", key: "axios" },
-  "socket.io": { name: "Socket.IO", key: "socket.io" },
-  "lodash": { name: "Lodash", key: "lodash" },
-  "underscore": { name: "Underscore.js", key: "underscore" },
-  "core-js": { name: "Core-js", key: "core-js" },
-  "tailwindcss": { name: "Tailwind CSS", key: "tailwindcss" },
-  "material-ui": { name: "Material-UI", key: "material-ui" },
-  "ant-design": { name: "Ant Design", key: "ant-design" },
-  "webpack": { name: "Webpack", key: "webpack" },
-  "babel": { name: "Babel", key: "babel" },
-  "typescript": { name: "TypeScript", key: "typescript" },
-  "sass": { name: "Sass", key: "sass" },
-  "less": { name: "Less", key: "less" },
-  "three.js": { name: "Three.js", key: "three.js" },
-  "animejs": { name: "Anime.js", key: "animejs" },
-  "gsap": { name: "GSAP", key: "gsap" },
-  "backbone": { name: "Backbone.js", key: "backbone" },
-  "ember": { name: "Ember.js", key: "ember" },
-  "p5": { name: "p5.js", key: "p5" },
-  "turbolinks": { name: "Turbolinks", key: "turbolinks" },
-  "crypto-js": { name: "CryptoJS", key: "crypto-js" },
-  "openssl": { name: "OpenSSL", key: "openssl" },
-  "node.js": { name: "Node.js", key: "node.js" },
-  "php": { name: "PHP", key: "php" },
-  "django": { name: "Django", key: "django" },
-  "laravel": { name: "Laravel", key: "laravel" },
-  "spring": { name: "Spring Framework", key: "spring" },
-  "ruby on rails": { name: "Ruby on Rails", key: "rails" },
-  "express": { name: "Express", key: "express" },
-  "flask": { name: "Flask", key: "flask" },
-  "matomo": { name: "Matomo Analytics", key: "matomo" },
-  "font awesome": { name: "Font Awesome", key: "fontawesome" },
-  "microsoft asp.net": { name: "ASP.NET", key: "asp.net" },
-  "hsts": { name: "HSTS", key: "hsts" },
-  "rxjs": { name: "RxJS", key: "rxjs" },
-  "immer": { name: "Immer", key: "immer" },
-  "recoil": { name: "Recoil", key: "recoil" },
-  "zustand": { name: "Zustand", key: "zustand" },
-  "formik": { name: "Formik", key: "formik" },
-  "yup": { name: "Yup", key: "yup" },
-  "joi": { name: "Joi", key: "joi" },
-  "react-query": { name: "React Query", key: "react-query" },
-  "react-router": { name: "React Router", key: "react-router" },
-  "next.js": { name: "Next.js", key: "next" },
-  "gatsby": { name: "Gatsby", key: "gatsby" },
-  "vite": { name: "Vite", key: "vite" },
-  "parcel": { name: "Parcel", key: "parcel" },
-  "rollup": { name: "Rollup", key: "rollup" },
-  "eslint": { name: "ESLint", key: "eslint" },
-  "prettier": { name: "Prettier", key: "prettier" },
-  "stylelint": { name: "Stylelint", key: "stylelint" },
-  "jest": { name: "Jest", key: "jest" },
-  "mocha": { name: "Mocha", key: "mocha" },
-  "chai": { name: "Chai", key: "chai" },
-  "cypress": { name: "Cypress", key: "cypress" },
-  "playwright": { name: "Playwright", key: "playwright" },
-  "puppeteer": { name: "Puppeteer", key: "puppeteer" },
-  "karma": { name: "Karma", key: "karma" },
-  "protractor": { name: "Protractor", key: "protractor" },
-  "nightwatch": { name: "Nightwatch", key: "nightwatch" },
-  "webdriverio": { name: "WebdriverIO", key: "webdriverio" },
-  "capybara": { name: "Capybara", key: "capybara" },
-  "selenium": { name: "Selenium", key: "selenium" },
-  "cucumber": { name: "Cucumber", key: "cucumber" },
-  "chai-http": { name: "Chai HTTP", key: "chai-http" },
-  "supertest": { name: "Supertest", key: "supertest" },
-  "nvd": { name: "NVD", key: "nvd" },
-  "endoflife.date": { name: "EndOfLife.date", key: "endoflife.date" },
-  "tailwind": { name: "Tailwind CSS", key: "tailwindcss" }
+  "jquery": true,
+  "jquery ui": true,
+  "jquery blockui": true,
+  "bootstrap": true,
+  "angular": true,
+  "react": true,
+  "vue": true,
+  "openssl": true,
+  "php": true,
+  "node.js": true
+  // el resto lo podés seguir agregando
 };
 
 // ======================= WORKER =======================
@@ -194,95 +29,75 @@ export async function onRequest({ request }) {
     const tech = techRaw.trim().toLowerCase();
     const version = versionRaw.trim();
 
-    // ======================= 1️⃣ END OF LIFE CHECK =======================
-    let eolData = null;
+    let status = "DESCONOCIDO";
+    let latest = "-";
+    let latestSupported = "-";
+    let cycle = null;
+
+    // ======================= END OF LIFE =======================
     try {
-      const eolRes = await fetch(`https://endoflife.date/api/${encodeURIComponent(tech)}.json`, { cf: { cacheTtl: 3600 } });
+      const eolRes = await fetch(`https://endoflife.date/api/${encodeURIComponent(tech)}.json`);
       if (eolRes.ok) {
-        eolData = await eolRes.json();
+        const eolData = await eolRes.json();
+
+        if (Array.isArray(eolData)) {
+          cycle = eolData.find(c => c.cycle && version.startsWith(String(c.cycle)));
+
+          const supported = eolData.find(c => !c.eol || new Date(c.eol) > new Date());
+          const latestEntry = eolData.find(c => c.latest);
+
+          latest = latestEntry?.latest || "-";
+          latestSupported = supported?.latest || "-";
+
+          if (cycle?.eol) {
+            status = new Date(cycle.eol) < new Date()
+              ? "FUERA DE SOPORTE"
+              : "CON SOPORTE";
+          }
+
+          if (status === "CON SOPORTE" && latestSupported !== "-" && version !== latestSupported) {
+            status = "DESACTUALIZADO";
+          }
+        }
       }
     } catch {}
 
-    let cycle = null;
-    let latest = null;
-    let latestSupported = null;
-    let status = "DESCONOCIDO";
-
-    // Validar contra catálogo primero
-    if (CATALOG[tech]) {
-      latest = CATALOG[tech].latest;
-      latestSupported = CATALOG[tech].supported;
-
-      if (version === latestSupported) {
-        status = "CON SOPORTE";
-      } else {
-        status = "DESACTUALIZADO";
-      }
-
-      // Si EOL data existe, verificar ciclo real
-      if (Array.isArray(eolData)) {
-        cycle = eolData.find(c => c.cycle && version.startsWith(String(c.cycle)));
-        if (cycle?.eol && new Date(cycle.eol) < new Date()) {
-          status = "FUERA DE SOPORTE";
-        }
-      }
-    } else if (Array.isArray(eolData)) {
-      cycle = eolData.find(c => c.cycle && version.startsWith(String(c.cycle)));
-      latest = eolData.find(c => c.latest)?.latest || null;
-      latestSupported = eolData.find(c => !c.eol || new Date(c.eol) > new Date())?.latest || null;
-
-      if (cycle?.eol) {
-        status = new Date(cycle.eol) < new Date() ? "FUERA DE SOPORTE" : "CON SOPORTE";
-      }
-
-      if (latest && latestSupported && version !== latestSupported) {
-        status = status === "CON SOPORTE" ? "DESACTUALIZADO" : status;
-      }
-    }
-
-    // ======================= 2️⃣ NVD CVE SEARCH =======================
+    // ======================= CVEs =======================
     let cves = [];
     try {
-      const cveRes = await fetch(`https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=${encodeURIComponent(tech)}&resultsPerPage=200`);
+      const cveRes = await fetch(
+        `https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=${encodeURIComponent(tech)}&resultsPerPage=200`
+      );
       if (cveRes.ok) {
         const cveJson = await cveRes.json();
         cves = (cveJson.vulnerabilities || [])
           .map(v => {
             const cve = v.cve;
             const metrics = cve.metrics || {};
-            const score = metrics.cvssMetricV31?.[0]?.cvssData || metrics.cvssMetricV30?.[0]?.cvssData || metrics.cvssMetricV2?.[0]?.cvssData;
+            const score =
+              metrics.cvssMetricV31?.[0]?.cvssData ||
+              metrics.cvssMetricV30?.[0]?.cvssData ||
+              metrics.cvssMetricV2?.[0]?.cvssData;
+
             return {
               id: cve.id,
               severity: score?.baseSeverity || "UNKNOWN",
               score: score?.baseScore || null,
               published: cve.published,
-              description: cve.descriptions?.[0]?.value || "",
               url: `https://nvd.nist.gov/vuln/detail/${cve.id}`
             };
-          })
-          .filter(c => c.description.toLowerCase().includes(tech) && c.description.includes(version));
+          });
       }
     } catch {}
-
-    // ======================= 3️⃣ ORDER & SUMMARY =======================
-    const order = { CRITICAL: 1, HIGH: 2, MEDIUM: 3, LOW: 4, UNKNOWN: 5 };
-    cves.sort((a, b) => order[a.severity] - order[b.severity]);
-    const summary = {
-      total: cves.length,
-      critical: cves.filter(c => c.severity === "CRITICAL").length,
-      high: cves.filter(c => c.severity === "HIGH").length
-    };
 
     return json({
       tecnologia: techRaw,
       version,
       estado: status,
-      latestVersion: latest || "-",
-      latestSupportedVersion: latestSupported || "-",
-      ciclo: cycle || null,
+      latestVersion: latest,
+      latestSupportedVersion: latestSupported,
+      ciclo: cycle,
       cves,
-      resumen: summary,
-      fuentes: ["https://endoflife.date", "https://nvd.nist.gov"],
       catalogUpdate: CATALOG_UPDATE_DATE
     });
 
@@ -292,5 +107,8 @@ export async function onRequest({ request }) {
 }
 
 function json(data, status = 200) {
-  return new Response(JSON.stringify(data), { status, headers: { "Content-Type": "application/json" } });
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { "Content-Type": "application/json" }
+  });
 }
